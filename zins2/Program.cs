f140c4d3791b4ba4639acf4cdc9a53c6c61f9a5c
Nino Meier <nino.meier@bene-edu.ch>
2023-11-16 17:30:46 +0100

    Add project files.

---
+++ zins2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        double Startkapital = 100000; 
+        const double Zinssatz = 0.11;
+        int Jahrezuberechnen = 10;
+        double zwischenzahl;
+        
+
+        for (int Jahr = 1; Jahr <= Jahrezuberechnen; Jahr++)
+        {
+            zwischenzahl = Startkapital * Zinssatz;
+            Startkapital = Startkapital - zwischenzahl;
+            Console.WriteLine($"Jahr {Jahr}: Kapital = {Startkapital:C2}");
+        }
+    }
+}
